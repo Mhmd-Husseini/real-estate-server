80a14eb430a80175c7d9f2e4901abb63a16ade5a
Husseini <mhmd.husseinyy@gmail.com>
2023-09-17 22:43:17 +0300

    creating book meeting api

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -1,7 +1,12 @@
 <?php
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
-use App\Models\User;
+use App\Models\Meeting;
+use DateTime;
+use DateInterval;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
+use DateTimeZone;
 
 class CalendarController extends Controller
 {
@@ -17,4 +22,29 @@ class CalendarController extends Controller
 
         return response()->json(['message' => 'Available time slots updated successfully']);
     }
+
+    
+    public function bookMeeting(Request $request)
+    {
+        $buyer = Auth::user();
+        $request->validate([
+            'seller_id' => 'required|exists:users,id',
+            'property_id' => 'required|exists:properties,id',
+            'date' => 'required|date',
+        ]);
+    
+        $meetingDateTime = new DateTime($request->input('date'), new DateTimeZone('EEST'));
+        $meetingDateTime->add(new DateInterval('PT3H'));
+        $formattedDate = $meetingDateTime->format('Y-m-d H:i:s');
+        $meeting = new Meeting([
+            'buyer_id' => $buyer->id,
+            'seller_id' => $request->input('seller_id'),
+            'property_id' => $request->input('property_id'),
+            'date' => $formattedDate,
+            'status' => "pending",
+        ]);
+        $meeting->save();
+        return response()->json(['message' => 'Meeting saved successfully'], 201);
+    }   
 }
+
