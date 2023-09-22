727134ed7ce04d6d8773baeb5df43eb810b5eaf9
Husseini <mhmd.husseinyy@gmail.com>
2023-09-22 17:28:37 +0300

    adding google_id in users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('role_id')->nullable();
+            $table->string('google_id')->nullable();;
             $table->foreign('role_id')->references('id')->on('roles')->onDelete('set null');
             $table->string('name');
             $table->string('email')->unique();
