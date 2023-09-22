e35e95ff21774e5a9d4c6f160a64b89ba2f52b1e
Husseini <mhmd.husseinyy@gmail.com>
2023-09-22 17:46:14 +0300

    adding google authentication routes

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,16 @@ use App\Http\Controllers\GuestController;
 use App\Http\Controllers\PropertyController;
 use App\Http\Controllers\CalendarController;
 
+Route::group(["prefix" => "guest"], function(){
+    Route::get("auth", [AuthController::class, "redirectToAuth"]);
+    Route::get('auth/callback', [AuthController::class, 'handleAuthCallback']);
+    Route::get("unauthorized", [AuthController::class, "unauthorized"])->name("unauthorized");
+    Route::post("login", [AuthController::class, "login"]);
+    Route::post("register", [AuthController::class, "register"]);
+    Route::get('properties/{id?}', [GuestController::class, 'getProperties']);
+    Route::get('trends', [GuestController::class, 'getTransactionsAndArticles']);
+});
+
 Route::group(["middleware" => "auth:api"], function(){
     
     Route::group(["middleware" => "auth.admin"], function(){
@@ -19,16 +29,9 @@ Route::group(["middleware" => "auth:api"], function(){
         Route::post("setAvailable", [CalendarController::class, "setAvailable"]);
         Route::post("bookMeeting", [CalendarController::class, "bookMeeting"]);
         Route::get("meetings", [CalendarController::class, "getMeetings"]);
-        Route::post("logout", [AuthController::class, "logout"]);
+        Route::get("profile", [AuthController::class, "profile"]);
     });
 
 });
 
-Route::group(["prefix" => "guest"], function(){
-    Route::get("unauthorized", [AuthController::class, "unauthorized"])->name("unauthorized");
-    Route::post("login", [AuthController::class, "login"]);
-    Route::post("register", [AuthController::class, "register"]);
-    Route::get('properties/{id?}', [GuestController::class, 'getProperties']);
-    Route::get('trends', [GuestController::class, 'getTransactionsAndArticles']);
-    Route::get("profile", [AuthController::class, "profile"]);
-});
+
