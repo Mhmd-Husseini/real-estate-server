2a6ecfcb49ab05bcd45d771aa306fbef35e61d59
Husseini <mhmd.husseinyy@gmail.com>
2023-09-23 23:27:43 +0300

    Node.js: creating get users api

--- server-Node.js/controllers/user.controller.js
+++ server-Node.js/controllers/user.controller.js
@@ -1,7 +1,11 @@
 const connection = require('./../configs/db.connection.js');
+const bcrypt = require("bcrypt");
 
-const hello = async (req, res) => {
-    res.send({ message: "hello" });
-  };  
+const getAllUsers = async (req, res) => {
+    connection.query("SELECT * FROM USERS WHERE role_id !=1", (err, result) => {
+      if (err) console.log(err)
+      res.send(result)
+    });
+  } 
 
-module.exports = { hello }
+module.exports = { getAllUsers }
--- server-Node.js/routes/user.routes.js
+++ server-Node.js/routes/user.routes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/user.controller");
 
-router.get("/", userController.hello)
+router.get("/", userController.getAllUsers)
 
 module.exports = router;
