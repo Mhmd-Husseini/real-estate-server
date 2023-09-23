40db6f8ec8740b54eeeb52aa76f8041da5f480f8
Husseini <mhmd.husseinyy@gmail.com>
2023-09-23 23:09:24 +0300

    Node.js: updating auth to restrict access to admin

--- server-Node.js/middlewares/auth.middleware.js
+++ server-Node.js/middlewares/auth.middleware.js
@@ -2,15 +2,23 @@ const jwt = require("jsonwebtoken");
 
 const authMiddleware = (req, res, next) => {
     const token = req.headers.authorization?.split(" ")[1];
-    if(!token) return res.status(401).send({message: "Unauthorized"});
+    
+    if (!token) {
+        return res.status(401).send({ message: "Unauthorized" });
+    }
+
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded
-        next()
+        if (decoded.role_id !== 1) {
+            return res.status(403).send({ message: "Forbidden. Insufficient privileges." });
+        }
+
+        req.user = decoded;
+        next();
     } catch (error) {
-        console.log(error);
-        res.status(401).send({message: "Unauthorized"})
+        console.error(error);
+        res.status(401).send({ message: "Unauthorized" });
     }
 }
 
-module.exports=authMiddleware;
+module.exports = authMiddleware;
