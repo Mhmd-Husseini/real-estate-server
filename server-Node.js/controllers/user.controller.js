94e0e592eb82b7ac0ce7f456e28ada2c56aa8d5d
Husseini <mhmd.husseinyy@gmail.com>
2023-09-23 23:34:55 +0300

    Node.js: creating get user api

--- server-Node.js/controllers/user.controller.js
+++ server-Node.js/controllers/user.controller.js
@@ -8,4 +8,14 @@ const getAllUsers = async (req, res) => {
     });
   } 
 
-module.exports = { getAllUsers }
+  const getUser = async (req, res) => {
+    const { id } = req.params;
+    console.log(id)
+    connection.query(`SELECT * FROM USERS WHERE id = ?`, [id], (err, result) => {
+      if (err) console.log(err)
+      if (result.length === 0) res.status(404).send({ message: "User not found" })
+      res.send(result[0])
+    })
+  }
+
+module.exports = { getAllUsers, getUser }
