7066774f7b009418c14d0f5f8a12e45298308ed5
Husseini <mhmd.husseinyy@gmail.com>
2023-09-23 23:57:09 +0300

    Node.js: creating update user api

--- server-Node.js/controllers/user.controller.js
+++ server-Node.js/controllers/user.controller.js
@@ -17,6 +17,7 @@ const getAllUsers = async (req, res) => {
       res.send(result[0])
     })
   }
+
   const createUser = async (req, res) => {
     const { role_id, password, name, phone, email } = req.body
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -31,4 +32,80 @@ const getAllUsers = async (req, res) => {
     })
   }
 
-module.exports = { getAllUsers, getUser, createUser }
+  const deleteUser = (req, res) => {
+    const { id } = req.params;
+    const sql = `DELETE FROM users WHERE id = ?`;
+    connection.query(sql, [id], (err, result) => {
+      if (err) {
+        console.error('Error deleting user:', err);
+        res.status(500).json({ error: 'An error occurred while deleting the user.' });
+      } else {
+        if (result.affectedRows > 0) {
+          res.status(200).json({ message: 'User deleted successfully.' });
+        } else {
+          res.status(404).json({ error: 'User not found.' });
+        }
+      }
+    });
+  };
+
+  const updateUser = async (req, res) => {
+    const { id, password, name, phone, email } = req.body;
+    const hashedPassword = password ? await bcrypt.hash(password, 10) : null;
+    const updateFields = [];
+    const queryParams = [];
+    
+    if (name) {
+      updateFields.push('name = ?');
+      queryParams.push(name);
+    }
+    if (email) {
+      updateFields.push('email = ?');
+      queryParams.push(email);
+    }
+    if (phone) {
+      updateFields.push('phone = ?');
+      queryParams.push(phone);
+    }
+    if (hashedPassword) {
+      updateFields.push('password = ?');
+      queryParams.push(hashedPassword);
+    }
+    
+    if (updateFields.length === 0) {
+      return res.status(400).json({ error: 'No fields to update' });
+    }
+    
+    connection.query('SELECT * FROM USERS WHERE id = ?', [id], (err, userResult) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Failed to check user existence' });
+      }
+
+      if (userResult.length === 0) {
+        return res.status(404).json({ error: 'User not found' });
+      }
+
+      queryParams.push(id);
+      const sql = `UPDATE USERS SET ${updateFields.join(', ')} WHERE id = ?`;
+      connection.query(sql, queryParams, (updateErr, result) => {
+        if (updateErr) {
+          console.error(updateErr);
+          return res.status(500).json({ error: 'Failed to update user' });
+        }
+        if (result.affectedRows > 0) {
+          connection.query('SELECT * FROM USERS WHERE id = ?', [id], (fetchErr, userResult) => {
+            if (fetchErr) {
+              console.error(fetchErr);
+              return res.status(500).json({ error: 'Failed to fetch updated user data' });
+            }
+            res.send(userResult[0]);
+          });
+        } else {
+          res.status(404).json({ error: 'User not found' });
+        }
+      });
+    });
+  }
+
+module.exports = { getAllUsers, getUser, createUser, deleteUser, updateUser }
--- server-Node.js/routes/user.routes.js
+++ server-Node.js/routes/user.routes.js
@@ -5,5 +5,7 @@ const userController = require("../controllers/user.controller");
 router.get("/", userController.getAllUsers)
 router.get("/:id", userController.getUser)
 router.post("/create", userController.createUser)
+router.delete("/delete/:id", userController.deleteUser)
+router.put("/update", userController.updateUser)
 
 module.exports = router;
