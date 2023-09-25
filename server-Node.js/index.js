455db15e4bc1a6968406f8af2a3a7c23c8c0d1d7
Husseini <mhmd.husseinyy@gmail.com>
2023-09-26 00:54:39 +0300

    Node.js: creating analytics api

--- server-Node.js/index.js
+++ server-Node.js/index.js
@@ -7,12 +7,14 @@ const connection = require("./configs/db.connection")
 require("dotenv").config()
 app.use(express.json())
 
-const authRouter = require("./routes/auth.routes")
 const authMiddleware = require("./middlewares/auth.middleware");
+const authRouter = require("./routes/auth.routes")
 const userRouter = require("./routes/user.routes")
+const analyticsRouter = require("./routes/analytics.routes")
 
 app.use("/", authRouter)
 app.use("/user", authMiddleware, userRouter)
+app.use("/analytics", authMiddleware, analyticsRouter)
 
 app.listen(8000, (err)=>{
     if(err){
